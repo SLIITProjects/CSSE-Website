ac64bdfd4c7c85fd8c88b6b9affca994a7950fe9
chethiya1996 <chethiya.gunawardana1996@gmail.com>
2018-10-04 02:01:01 +0530

    routes data retrival class

---
+++ RoutesData.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chethiya
+ * Date: 10/4/18
+ * Time: 1:54 AM
+ */
+
+require_once './vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+class RoutesData{
+
+    protected $database;
+    protected $dbname = 'Accounts';
+
+    public function __construct(){
+
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/secret/csse-ee784-62d164010846.json');
+        $firebase = (new factory)->withServiceAccount($serviceAccount)->create();
+
+        $this->database = $firebase->getDatabase();
+
+    }
+
+    public function get(){
+
+
+        if($this->database->getReference($this->dbname)->getSnapshot()){
+
+            return $this->database->getReference($this->dbname)->getValue();
+        }
+        else{
+            return FALSE;
+        }
+    }
+
+}
